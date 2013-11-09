2f67a63a61bb086b87a2ff7537c922ffa2c159de
MT10.8.5 <MT10.8.5@moon.com>
2013-11-09 14:58:04 +0800

    初始化

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = 'e346b6523b4e17386a909b259be2a54714a10f4a8acaa7d98bf56a30f9336100377f2ef9a87e3cffd85beb87b6713bb43ce46088f03c46228ee93a7b54bd2616'
